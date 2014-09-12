c1815a896899776501feb54ec16be1effd37c552
swihart <bruce.swihart@gmail.com>
2014-09-12 11:52:59 -0400

    making a package y'all

---
+++ R/_00_prep_data.R
@@ -0,0 +1,17 @@
+#' Prep the Berkely Growth Study Data
+#'
+#' This function allows you to express your love of cats.
+#' @param love Do you love cats? Defaults to TRUE.
+#' @keywords cats
+#' @export
+#' @examples
+#' cat_function()
+
+prep_data <- function(love=TRUE){
+  if(love==TRUE){
+    print("I love cats!")
+  }
+  else {
+    print("I am not a cool person.")
+  }
+}
---
+++ R/wfpca-package.r
@@ -0,0 +1,5 @@
+#' wfpca.
+#'
+#' @name wfpca
+#' @docType package
+NULL
