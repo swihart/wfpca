913038d707bd282284de9ae32b58c76534e00b49
swihart <bruce.swihart@gmail.com>
2014-10-18 20:38:35 -0400

    add refund to required packages in DESCRIPTION and add slope, intercept arguments to calculate_ses()

--- R/calculate_ses.R
+++ R/calculate_ses.R
@@ -5,6 +5,10 @@
 #' and the final height in column 32 of data_in.  In future I will build in
 #' params for the 100 and the 12 and the rnorm in teh SES assignment.
 #' @param data_in an object returned from sample_data()
+#' @param slope a number that determines the linear relationship of final height and SES.
+#' For instance, if slope=100 and intercept=12, then the SES for individual i is 
+#' SES(i) = 100*( height(i) - minimum of all heights )/(max of all heights - min of all heights) + 12 + rnorm(1)
+#' @param intercept a number, see explanation of slope
 #' @keywords oversampling
 #' @export
 #' @examples
@@ -13,14 +17,14 @@
 #' over_samp_mat<-sample_data(d,1000)
 #' with_ses <- calculate_ses(over_samp_mat)
 #' 
-calculate_ses <- function(data_in=NULL){
+calculate_ses <- function(data_in=NULL, slope=100, intercept=12){
   ## @knitr SESHeight
   ## induce linear relationship between SES and Height,
   ## that ranges between 12 and 112.  We can suppose that this SES is household income
   ## in 1000 USD or something like that.  Then add to overSampMat (now it is 33rd column)
-  SES <- 100*(data_in[,32]-min(data_in[,32]))/
-             (max(data_in[,32])-min(data_in[,32])) + 12 +
-             rnorm(dim(data_in)[1])
+  SES <- slope*(data_in[,32]-min(data_in[,32]))/
+               (max(data_in[,32])-min(data_in[,32])) + intercept +
+               rnorm(dim(data_in)[1])
   data_in$ses <- SES
   data_in
 }
